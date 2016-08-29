369a1b279b0022e2f433f4a4fc5233dcf4e52b81
Jeff Wilcox <jwilcox@microsoft.com>
2016-08-29 13:43:38 -0700

    moving app up

--- deploy.cmd
+++ deploy.cmd
@@ -208,7 +208,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%%NODE_APPLICATION_DIRECTORY%\Gruntfile.js" (
 
 :: 8. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\app" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
