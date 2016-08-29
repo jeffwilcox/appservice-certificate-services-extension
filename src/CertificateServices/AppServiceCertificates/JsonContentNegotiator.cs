f933f16219793f5359cd59a090aeac319f011cad
Jeff Wilcox <jwilcox@microsoft.com>
2016-08-29 13:01:25 -0700

    Adding files

---
+++ src/CertificateServices/AppServiceCertificates/JsonContentNegotiator.cs
@@ -0,0 +1,34 @@
+﻿//
+// Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace AppService.CertificateServices
+{
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+        private const string JsonType = "application/json";
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+        }
+
+        public ContentNegotiationResult Negotiate(
+                Type type,
+                HttpRequestMessage request,
+                IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(
+                _jsonFormatter,
+                new MediaTypeHeaderValue(JsonType));
+        }
+    }
+}
